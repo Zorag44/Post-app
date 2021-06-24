fb9ad6f69d5ab4522ddddf0f8f699536dee33cc7
Zorag44 <shromworld@gmail.com>
2021-06-24 21:06:01 +0530

    added something

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +134,4 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+django_heroku.settings(locals())
