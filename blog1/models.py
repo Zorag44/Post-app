0626e0b431d245267b3cc4224e8210e9225f8452
Zorag44 <shromworld@gmail.com>
2021-06-25 13:27:17 +0530

    pro

--- blog1/models.py
+++ blog1/models.py
@@ -18,7 +18,7 @@ class post(models.Model):
 
 class profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
-    image=models.ImageField(default='default.png', upload_to='media/profile_pics')
+    image=models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} profile'
