d8433fc5e03394b2cc42f847bf822f066e06a6b7
Zorag44 <shromworld@gmail.com>
2021-06-28 10:32:00 +0530

    maanja

--- blog/urls.py
+++ blog/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('blog1.urls')),
     re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT})
 ]
-# if settings.DEBUG==False:
-#     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG==False:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
